eef3ab7a136b090285fc62995a5ab079b2177890
Carlos <cmelero0808@gmail.com>
2023-03-06 13:15:09 +0100

    Metido un condicionales

--- ej1.py
+++ ej1.py
@@ -1,4 +1,5 @@
 nombre = input("Introduce tu nombre: ")
+valor = 8
 
 '''
     Comentar de conflicto yo que sé
@@ -6,6 +7,13 @@ nombre = input("Introduce tu nombre: ")
 
 #Comentario de una linea
 def hola(nombre_recibido) :
+    if valor < 4:
+        print("Hola")
+    elif  valor >= 4 and valor < 9:
+        print("Adios")
+    else:
+        print("Buenos días")
+
     print("Hola mundo " + nombre_recibido)
     return 1
 
